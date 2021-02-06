9eedebb2c36edb6854ebef21f472a3ba83791725
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-02-05 22:39:57 -0800

    Add UI Logic and html

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -5,11 +5,11 @@ describe('Calculator', () => {
 
   beforeEach(() => {
     const planets = ["mercury", "venus", "earth", "mars", "jupiter"];
-    reusableCalculator = new Calculator(45,"caucasian", "female", planets);
+    reusableCalculator = new Calculator(45, "caucasian", "female", planets);
   });
 
   test('should correctly call construtor with arguments', () => {
-    expect(reusableCalculator).toEqual({userInputAge: 45, race: "caucasian", gender: "female", planets: ["mercury", "venus", "earth", "mars", "jupiter"], agePerPlanet: {}, planetId: -1});
+    expect(reusableCalculator).toEqual({ userInputAge: 45, race: "caucasian", gender: "female", planets: ["mercury", "venus", "earth", "mars", "jupiter"], agePerPlanet: {}, planetId: -1 });
   });
 
   test('should correctly call method "assignPlanetId()"', () => {
@@ -19,19 +19,18 @@ describe('Calculator', () => {
   });
 
   test('should correctly call method "addPlanet()"', () => {
-    const planetObjectRec = {agePlanetBased: 187, estLifeExp: 277};
-    const planetObjectExp = {mercury: {agePlanetBased: 187, estLifeExp: 277}};
-    reusableCalculator.addPlanet( planetObjectRec);
+    const planetObjectRec = { agePlanetBased: 187, estLifeExp: 277 };
+    const planetObjectExp = { mercury: { agePlanetBased: 187, estLifeExp: 277 } };
+    reusableCalculator.addPlanet(planetObjectRec);
     expect(reusableCalculator.agePerPlanet).toMatchObject(planetObjectExp);
-   });
+  });
 
-   test('should correctly call method "addAgeOnPlanets()"', () => {
-    const agePlanetBasedAdjust = {mercury: 0.24,venus: 0.62, earth: 1.00, mars: 1.88, jupiter: 11.86};
-    const raceBasedLifeExpOnEarth = {caucasian: 65, asian: 70, africanAmerican: 65, latino: 4};
-    const result = {mercury: { agePlanetBased: 187, estLifeExp: 277 }, venus: { agePlanetBased: 72, estLifeExp: 111 },earth: { agePlanetBased: 45, estLifeExp: 72 }, mars: { agePlanetBased: 23, estLifeExp: 41 }, jupiter: { agePlanetBased: 3, estLifeExp: 12 } };
+  test('should correctly call method "addAgeOnPlanets()"', () => {
+    const agePlanetBasedAdjust = { mercury: 0.24, venus: 0.62, earth: 1.00, mars: 1.88, jupiter: 11.86 };
+    const raceBasedLifeExpOnEarth = { caucasian: 65, asian: 70, africanAmerican: 65, latino: 4 };
+    const result = { mercury: { agePlanetBased: 187, estLifeExp: 277 }, venus: { agePlanetBased: 72, estLifeExp: 111 }, earth: { agePlanetBased: 45, estLifeExp: 72 }, mars: { agePlanetBased: 23, estLifeExp: 41 }, jupiter: { agePlanetBased: 3, estLifeExp: 12 } };
     reusableCalculator.addAgeOnPlanets(agePlanetBasedAdjust, raceBasedLifeExpOnEarth);
     expect(reusableCalculator.agePerPlanet).toMatchObject(result);
-   });
-
+  });
 
 });
--- src/main.js
+++ src/main.js
@@ -7,7 +7,29 @@ import Calculator from './js/Calculator';
 
 $(document).ready(function() {
   $('#formOne').submit(function(event) {
+    event.preventDefault();
+    $("#output").hide();
+    var buttonObj = document.querySelector("button");
+    buttonObj.textContent = "submitted";
+    $("#toppingsDiv").hide();
     
+    const userEnteredAge = parseInt($("#age").val());
+    const userEnteredSex =$("input:radio[name=sex]:checked").val();
+    const userEnteredRace = $("input:radio[name=race]:checked").val();
+  
+    const agePlanetBasedAdjust = {mercury: 0.24,venus: 0.62, earth: 1.00, mars: 1.88, jupiter: 11.86};
+    const raceBasedLifeExpOnEarth = {caucasian: 65, asian: 70, africanAmerican: 65, latino: 4}; //assumed values based on Demographic
+    const planets = ["mercury", "venus", "earth", "mars", "jupiter"];
+    const calculator = new Calculator(userEnteredAge, userEnteredRace, userEnteredSex, planets);
+    calculator.addAgeOnPlanets(agePlanetBasedAdjust, raceBasedLifeExpOnEarth);
+    let response = calculator.agePerPlanet;
+    console.log(response);
+    $("#output").show();
+    const raceNameCapitalized = userEnteredRace.charAt(0).toUpperCase() + userEnteredRace.slice(1);
+    const sexCapitalized = userEnteredSex.charAt(0).toUpperCase() + userEnteredSex.slice(1);
+    let yearsLeftSyntax = `<span class="greyColor">${userEnteredAge}</span> : <span class="greyColor">${sexCapitalized}</span> : <span class="greyColor">${raceNameCapitalized}</span>`;
+    $("#ageSecRace").append(yearsLeftSyntax);
+    $('#response').append("<p>" + response + "</p>");
   });
 
 });
