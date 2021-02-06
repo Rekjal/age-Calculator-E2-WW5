587d2078bfdbc24c027cccaf1ff0a6cc9d2b7b72
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-02-05 20:59:30 -0800

    Call Calculator class contructor with arguments

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -4,11 +4,15 @@ describe('Calculator', () => {
   let reusableCalculator;
 
   beforeEach(() => {
-    reusableCalculator = new Calculator();
+    const agePlanetBasedAdjust = {mercury: 0.24,venus: 0.62, earth: 1.00, mars: 1.88, jupiter: 11.86};
+    const raceBasedLifeExpOnEarth = {caucasian: 65, asian: 70, africanAmerican: 65, latino: 4};
+    const planets = ["mercury", "venus", "earth", "mars", "jupiter"];
+    reusableCalculator = new Calculator(45,"caucasian", "female", planets);
+  });
+ 
+  test('should correctly call construtor with arguments', () => {
+    expect(reusableCalculator).toEqual({userInputAge: 45, race: "caucasian", gender: "female", planets: ["mercury", "venus", "earth", "mars", "jupiter"], agePerPlanet: {}, planetId: -1});
   });
 
-  test('should correctly call "Calculator" class construtor with no arguments', () => {
-    expect(reusableCalculator).toEqual({});
-  });
   
 });
--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -1,6 +1,10 @@
 export default class Calculator {
-  constructor() {
-
+  constructor(userInputAge, race, gender, planets) {
+    this.userInputAge = userInputAge;
+    this.race = race;
+    this.gender = gender;
+    this.planets = planets;
+    this.agePerPlanet = {};
+    this.planetId = -1;
   }
-
 }
